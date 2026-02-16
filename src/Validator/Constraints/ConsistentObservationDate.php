d347c2170c600c0c0637e76485eeb995c9ea6f7f
William FOHOM <william.fohom@belgocam.com>
2026-02-16 23:45:03 +0100

    Mise à jour du 16.02.2026

--- src/Validator/Constraints/ConsistentObservationDate.php
+++ src/Validator/Constraints/ConsistentObservationDate.php
@@ -6,6 +6,7 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute]
 class ConsistentObservationDate extends Constraint
 {
+    public string $message = "La date d'observation doit être comprise dans une plage de temps raisonnable autour de la date de visite."; // Message générique
     public string $messageFuture = "La date d'observation ne peut pas être dans le futur.";
     public string $messageTooOld = "Impossible d'ajouter une observation antérieure à la visite elle-même.";
     
